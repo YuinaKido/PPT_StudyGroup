8b6dba9f88b335d65eed8ae8467d9ceba956c054
yuichi.imai1 <yuichi.imai1@persol.co.jp>
2019-03-01 21:05:45 +0900

    プロセルキル系のやつ_今井分

---
+++ kill_process/killProcess_imai.ps1
@@ -0,0 +1,16 @@
+﻿$ws = New-Object -com Wscript.Shell
+
+$test = Get-Process | Out-GridView -PassThru
+
+[void][System.Reflection.Assembly]::Load("Microsoft.VisualBasic, Version=8.0.0.0, Culture=Neutral, PublicKeyToken=b03f5f7f11d50a3a")
+
+
+$test2 = $test| out-string
+
+#　インプットボックスの表示
+$INPUT = [Microsoft.VisualBasic.Interaction]::InputBox($test2, "タイトル")
+
+Write-Output $test
+
+
+$ws.Popup($test2,0,"曲名",0)
